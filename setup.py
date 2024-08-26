bf4565bb7055f7d83d4995479d1b2c801a2111e2
sstbrg2 <sstbrg@gmail.com>
2024-08-26 12:03:53 +0300

    fixed setup.pu

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='biot_python_sdk',
-    version='1.0.0',
+    version='1.0.2',
     description='A python SDK that wraps Bio-T Open API',
     author='Stanislav Steinberg',
     author_email='sstbrg@gmail.com',
     url='https://github.com/sstbrg/biot_python_sdk',
-    py_modules=['biot'],
+    py_modules=['biot_python_sdk'],
     install_requires=[
         'requests',
     ],
